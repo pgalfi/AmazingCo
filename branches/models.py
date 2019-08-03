92b3a7c6ead29414bbc65a39b38686418870a0e4
Peter Galfi <pgalfi@gmail.com>
2019-08-03 19:04:53 +0200

    test data and further data model tests

--- branches/models.py
+++ branches/models.py
@@ -4,21 +4,21 @@ from django.db import models
 
 class Branch(models.Model):
     name = models.CharField(max_length=100)
-    node_path = models.TextField(db_index=True)
+    up_path = models.TextField(db_index=True)
     parent = models.ForeignKey('Branch', default=None, null=True, on_delete=models.CASCADE, related_name="children")
     root = models.ForeignKey('Branch', default=None, null=True, on_delete=models.CASCADE, related_name="all_nodes")
     height = models.IntegerField(default=0)
 
     @property
     def full_path(self):
-        return self.node_path + self.name
+        return self.up_path + self.name
 
     def __repr__(self):
-        return self.name + "=" + self.node_path + " (" + str(self.height) + ")"
+        return self.name + "=" + self.up_path + " (" + str(self.height) + ")"
 
     def add_child(self, child_name):
         child = Branch(name=child_name, parent=self, root=self.root if self.root is not None else self,
-                       height=self.height + 1, node_path=self.node_path + self.name + "/")
+                       height=self.height + 1, up_path=self.up_path + "/" + str(self.id))
         child.save()
         return child
 
--- branches/tests/test_branch.py
+++ branches/tests/test_branch.py
@@ -6,13 +6,15 @@ from branches.models import Branch
 class TestBranch(TestCase):
 
     def test_setup_branches(self):
-        root = Branch(parent=None, root=None, height=0, node_path="")
+        root = Branch(parent=None, root=None, height=0, up_path="")
         root.save()
         abranch = root.add_child("a")
+        self.assertEqual("/1", abranch.up_path)
         achildren = abranch.add_child("c"), abranch.add_child("d"), abranch.add_child("e")
+        self.assertEqual("/1/" + str(abranch.id), achildren[0].up_path)
         bbranch = root.add_child("b")
 
         self.assertEqual(6, Branch.objects.all().count())
         self.assertEqual(2, Branch.objects.get(name="d").height)
-        self.assertEqual("/a/", Branch.objects.get(name="d").node_path)
+        self.assertEqual("/1/" + str(abranch.id), Branch.objects.get(name="d").up_path)
 
